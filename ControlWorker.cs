796bd04c29a31493aff56ce39b1779bbb87ea30c
Isochre <isochre@gmail.com>
2025-01-05 15:59:05 +0100

    hg

---
+++ ControlWorker.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace PowerControl
+{
+    public class ControlWorker(ILogger<ControlWorker> logger) : BackgroundService
+    {
+        Stopwatch debugStopwatch = new Stopwatch();
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (logger.IsEnabled(LogLevel.Information))
+                {
+                    if (debugStopwatch.ElapsedMilliseconds > 300000) //toutes les 5min on met à jour artificielement l'état de l'unité de contrôle
+                    {
+                        debugStopwatch.Restart();
+                        Console.WriteLine("debugStopwatch elapsed");
+                    }
+                }
+                await Task.Delay(10, stoppingToken);
+            }
+        }
+    }
+}
+
