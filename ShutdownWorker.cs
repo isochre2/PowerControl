7214eed911a2ebafd87656e1a700a859883eed12
isochre <isochre@gmail.com>
2026-01-01 22:22:56 +0100

    Gestion de l'arrêt du PowerMonitorControl

--- ShutdownWorker.cs
+++ ShutdownWorker.cs
@@ -1,52 +1,97 @@
+using System.ComponentModel.DataAnnotations;
 using Renci.SshNet;
 using System.Diagnostics;
 using System.Device.Gpio;
+using Iot.Device.Camera.Settings;
 using ConnectionInfo = Renci.SshNet.ConnectionInfo;
 
 namespace PowerControl.Services;
 
 public class ShutdownWorker : BackgroundService
 {
-    private SshClient _sshClient;
+    private class LocalSSHClient
+    {
+        public SshClient SSHClient { get; set; }
+
+        private string PrivateKeyPath => "/app/ssh_keys/id_rsa_shutdown";
+
+        [Required]
+        public string HostName { get; set; }
+
+        [Required]
+        public string User { get; set; }
+
+        public bool Connect()
+        {
+            try
+            {
+                var keyFile = new PrivateKeyFile(PrivateKeyPath);
+                var keyAuth = new PrivateKeyAuthenticationMethod(User, keyFile);
+                var connectionInfo = new ConnectionInfo(HostName, User, keyAuth);
+                SSHClient = new SshClient(connectionInfo);
+                SSHClient.Connect();
+                Console.WriteLine($"Connexion SSH établie à {HostName} avec succès !");
+                ExecuteCommand("echo \"Connexion établie le $(date)\" >> shutdown_log.txt",
+                    out string CommandOutputType, out string errorOutput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors de la connexion SSH à {HostName} : {ex.Message}");
+            }
+
+            return true;
+        }
+
+        public bool ExecuteCommand(string command, out string commandOutput, out string errorOutput)
+        {
+            errorOutput = "";
+            commandOutput = "";
+            if (SSHClient == null || !SSHClient.IsConnected)
+            {
+                errorOutput = "Client SSH non connecté";
+                return false;
+            }
+
+            var cmd = SSHClient.RunCommand(command);
+            commandOutput = cmd.Result;
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            if (SSHClient != null && SSHClient.IsConnected)
+            {
+                SSHClient.Disconnect();
+                Console.WriteLine("Connexion SSH fermée.");
+            }
+        }
+    }
+
+    private LocalSSHClient RaspberryControl => new() { HostName = "raspberrypicontrol", User = "isochre" };
+    private LocalSSHClient RaspberryPower => new() { HostName = "raspberrypi", User = "isochre" };
+
 
     private static GpioController gpioController;
 
     private readonly ILogger<ControlWorker> logger;
-
-    public static bool initialized = false;
-
-    Stopwatch debugStopwatch = new Stopwatch();
 
     private const int SHUTDOWN_GPIO = 24 /*25*/;
 
     public ShutdownWorker(ILogger<ControlWorker> _logger)
     {
         logger = _logger;
-        debugStopwatch.Start();
-        try
-        {
-            InitGpios();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Impossible d'initialiser les GPIOs de contrôle : \n" + e.Message);
-            //throw;
-        }
-    }
 
-    private void InitGpios()
-    {
-        gpioController = new GpioController();
-        gpioController.OpenPin(SHUTDOWN_GPIO, PinMode.InputPullUp);
-        Connect("raspberrypi", "isochre");
-        
-        initialized = true;
-        
+        InitGpios();
+
         var cts = new CancellationTokenSource();
         Task.Run(async () =>
         {
             while (!cts.IsCancellationRequested)
             {
+                if (RaspberryControl?.SSHClient?.IsConnected is false) RaspberryControl.Connect();
+                if (RaspberryPower?.SSHClient?.IsConnected is false) RaspberryPower.Connect();
+
+
                 Console.WriteLine("GPIO " + SHUTDOWN_GPIO + " : " + gpioController.Read(SHUTDOWN_GPIO));
                 Console.WriteLine("------------------------------------------------------");
                 await Task.Delay(1000, cts.Token);
@@ -54,43 +99,19 @@ public class ShutdownWorker : BackgroundService
         }, cts.Token);
     }
 
-    public void Connect(string host, string username)
+    private void InitGpios()
     {
         try
         {
-            string privateKeyPath = "/app/ssh_keys/id_rsa_shutdown";
-            var keyFile = new PrivateKeyFile(privateKeyPath);
-            var keyAuth = new PrivateKeyAuthenticationMethod(username, keyFile);
-            var connectionInfo = new ConnectionInfo(host, username, keyAuth);
-            _sshClient = new SshClient(connectionInfo);
-            _sshClient.Connect();
-            Console.WriteLine($"Connexion SSH établie à {host} avec succès !");
+            gpioController = new GpioController();
+            gpioController.OpenPin(SHUTDOWN_GPIO, PinMode.InputPullUp);
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            Console.WriteLine($"Erreur lors de la connexion SSH : {ex.Message}");
+            Console.WriteLine("Impossible d'initialiser les GPIOs de contrôle : \n" + e.Message);
         }
     }
 
-    public void Disconnect()
-    {
-        if (_sshClient != null && _sshClient.IsConnected)
-        {
-            _sshClient.Disconnect();
-            Console.WriteLine("Connexion SSH fermée.");
-        }
-    }
-
-    public string ExecuteCommand(string command)
-    {
-        if (_sshClient != null && _sshClient.IsConnected)
-        {
-            var cmd = _sshClient.RunCommand(command);
-            return cmd.Result;
-        }
-
-        return "Non connecté.";
-    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -98,12 +119,21 @@ public class ShutdownWorker : BackgroundService
         {
             if (logger.IsEnabled(LogLevel.Information))
             {
-                debugStopwatch.Restart();
                 //On vérifie l'état de 
                 if (gpioController.Read(SHUTDOWN_GPIO) == PinValue.Low)
                 {
-                    var result = ExecuteCommand("echo \"Commande d'arrêt reçue le '$(date)\" >> shutdown_log.txt && sudo shutdown -h now");
-                    Console.WriteLine(result);
+                    var shutdownCommandResult = RaspberryPower.ExecuteCommand(
+                        "echo \"Commande d'arrêt reçue le $(date)\" >> shutdown_log.txt && sudo shutdown -h now",
+                        out string errorOutputPower,
+                        out string commandOutputPower);
+                    Console.WriteLine(errorOutputPower);
+                    if (shutdownCommandResult)
+                    {
+                        RaspberryControl.ExecuteCommand(
+                            "echo \"Commande d'arrêt reçue le $(date)\" >> shutdown_log.txt && sudo shutdown -h now",
+                            out string errorOutputControl,
+                            out string commandOutputControl);
+                    }
                 }
 
                 await Task.Delay(100, stoppingToken);
